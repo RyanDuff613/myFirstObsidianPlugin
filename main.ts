fb6e3fb373cf4ab5489be021d393c74f6a0bf20e
Ryan Duff <ryanduff613@gmail.com>
2025-03-07 22:31:55 -0800

    remove attempt to open Obsidian > Settings menu

--- main.ts
+++ main.ts
@@ -2,10 +2,10 @@ import { Notice, Plugin } from 'obsidian';
 
 export default class myFirstPlugin extends Plugin {
 	async onload() {
-		new Notice("myFirstPlugin loaded!");
-		this.addRibbonIcon("eye","myFirstPlugin notice trigger",() => {
+		this.addRibbonIcon("cog","myFirstPlugin notice trigger",() => {
 			new Notice("Hello from myFirstPlugin");
 		})
+		
 	}
 
 	onunload() {
